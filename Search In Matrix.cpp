10bafaf701b0ed7bf474c519a1b9a829db57fa0f
Krym <gumimash@gmail.com>
2024-06-20 16:08:10 +0200

    search matrix

---
+++ Search In Matrix.cpp
@@ -0,0 +1,34 @@
+    #include <iostream>
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main()
+    {
+        int n,m, x;
+        cin >> n;
+        cin >> m;
+        
+        vector<vector<int>>a(n, vector<int>(m));
+     
+        for (int i = 0; i < n; i++) {
+            for(int j = 0;j<m;j++){
+                
+                cin >> a[i][j];
+            }
+        }
+        
+        
+        cin>> x;
+        
+         for (int i = 0; i < n; i++) {
+            for(int j = 0;j<m;j++){
+                if(a[i][j] == x){
+                    cout<<"will not take number";
+                    return 0;
+                }
+            }
+        }
+        cout<<"will take number";
+     
+    return 0;
+    }
